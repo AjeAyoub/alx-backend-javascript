507b558fd91b9753c03515261621ebdbe162c739
AjeAyoub <ajeddabayoub.official@gmail.com>
2024-05-29 12:06:58 -0700

    All tasks

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,20 +1,15 @@
-// 0-constants.js
-
-// Function taskFirst using const
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  // Function getLast as provided
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  // Function taskNext using let
-  export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
